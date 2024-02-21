60cb126d3e6520639d5f9b89613f109791fb4c9d
nothowstorygoes <esposito.pioalessandro@gmail.com>
2024-02-21 16:57:00 +0100

    fixed imports

--- src/app/components/subComponents/navbar.js
+++ src/app/components/subComponents/navbar.js
@@ -1,13 +1,14 @@
 // Navbar.jsx
 import React from 'react';
 import styles from './navbar.module.css';
+import Image from 'next/image';
 
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbarContainer}>
         <ul className={styles.navbarMenu}>
-        
+        <Image src="/notes-in-cloud/icons/logo/logo.png" alt="Logo" className={styles.navbarLogo} />
           <li className={styles.navbarItem}>
           <a href="/notes-in-cloud/components/home" className={styles.navbarLink}>Home</a>
           </li>
