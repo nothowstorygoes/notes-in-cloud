4ea9e5264c5ec5e5b44a856c41dffc6f13c57144
nothowstorygoes <esposito.pioalessandro@gmail.com>
2024-02-21 23:59:37 +0100

    Initial commit for responsiveness

--- src/app/components/subComponents/navbar.js
+++ src/app/components/subComponents/navbar.js
@@ -1,24 +1,37 @@
 // Navbar.jsx
-import React from 'react';
-import styles from './navbar.module.css';
-import Image from 'next/image';
-
+import React from "react";
+import styles from "./navbar.module.css";
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbarContainer}>
-      <p className={styles.logo}>Notes in Cloud</p>
+        <p className={styles.logo}>Notes in Cloud</p>
         <ul className={styles.navbarMenu}>
-        
           <li className={styles.navbarItem}>
-          <a href="/notes-in-cloud/components/home" className={styles.navbarLink}>Home</a>
+            <a
+              href="/notes-in-cloud/components/home"
+              className={styles.navbarLink}
+            >
+              Home
+            </a>
           </li>
-          
-          
+          <li className={styles.icons}>
+            <a href="/notes-in-cloud/components/home">
+              <img src="/notes-in/cloud/icons/home.png"/>
+            </a>
+            <a href="/notes-in-cloud/components/profile">
+              <img src="/notes-in/cloud/icons/profile.png"/>
+            </a>
+          </li>
           <li className={styles.navbarItem}>
-          <a href="/notes-in-cloud/components/profile" className={styles.navbarLink}>Profile</a>
+            <a
+              href="/notes-in-cloud/components/profile"
+              className={styles.navbarLink}
+            >
+              Profile
+            </a>
           </li>
-          
+
           {/* Add more navbar items as needed */}
         </ul>
       </div>
