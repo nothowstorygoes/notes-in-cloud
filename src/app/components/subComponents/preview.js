fcb6ffcbd10f8c2e05e190ff9a257a06ff6a8e0e
nothowstorygoes <esposito.pioalessandro@gmail.com>
2024-02-24 12:53:32 +0100

    Fixed handleDelete

--- src/app/components/subComponents/preview.js
+++ src/app/components/subComponents/preview.js
@@ -9,7 +9,7 @@ import {
   ref,
   uploadBytesResumable,
   getDownloadURL,
-  deleteObject
+  deleteObject,
 } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 import app from "../../firebase";
@@ -29,7 +29,6 @@ export default function Preview({
   const fileInputRef = useRef(null); // Add a ref to the file input
   const [coverImageURL, setCoverImageURL] = useState(null);
   const [isLoading, setIsLoading] = useState(true); // Add a loading state
-
 
   useEffect(() => {
     const checkForCoverImage = async () => {
@@ -55,72 +54,91 @@ export default function Preview({
     return <div className={styles.load}>Loading...</div>; // Render a loading indicator while checking for cover image
   }
 
- const fetchFile = async (fileName) => {
-   // Fetch the updated match.json file
-   const matchJsonRef = ref(storage, `Userdata/${user.uid}/match.json`);
-   const matchJsonURL = await getDownloadURL(matchJsonRef);
-   const response = await fetch(matchJsonURL);
-   const matchJson = await response.json();
- 
-   // Find the updated object in match.json
-   const match = matchJson.find(item => item.pdf === fileName);
-   if (match) {
-     // Update the state with the new cover image
-     setFile((prevFile) => {
-       if (prevFile.name === fileName) {
-         return { ...prevFile, cover: match.cover };
-       }
-       return prevFile;
-     });
-   } else {
-     // If there is no match, it means the cover image has been deleted
-     // Update the state to reflect that
-     setFile((prevFile) => {
-       if (prevFile.name === fileName) {
-         return { ...prevFile, cover: "null" };
-       }
-       return prevFile;
-     });
-   }};
+  const fetchFile = async (fileName) => {
+    // Fetch the updated match.json file
+    const matchJsonRef = ref(storage, `Userdata/${user.uid}/match.json`);
+    const matchJsonURL = await getDownloadURL(matchJsonRef);
+    const response = await fetch(matchJsonURL);
+    const matchJson = await response.json();
 
-   const handleDelete = async () => {
-     onDelete(file);
-     const matchJsonRef = ref(storage, `Userdata/${user.uid}/match.json`);
-     const matchJsonURL = await getDownloadURL(matchJsonRef);
-     const response = await fetch(matchJsonURL);
-     let matchJson = await response.json();
+    // Find the updated object in match.json
+    const match = matchJson.find((item) => item.pdf === fileName);
+    if (match) {
+      // Update the state with the new cover image
+      setFile((prevFile) => {
+        if (prevFile.name === fileName) {
+          return { ...prevFile, cover: match.cover };
+        }
+        return prevFile;
+      });
+    } else {
+      // If there is no match, it means the cover image has been deleted
+      // Update the state to reflect that
+      setFile((prevFile) => {
+        if (prevFile.name === fileName) {
+          return { ...prevFile, cover: "null" };
+        }
+        return prevFile;
+      });
+    }
+  };
 
-     // Find the index of the matching object
-     const matchIndex = matchJson.findIndex((item) => item.pdf === file.name);
-     if (matchIndex !== -1) {
-       // Remove the matching object from the matchJson array
-       matchJson.splice(matchIndex,  1);
+  const handleDelete = async () => {
+    onDelete(file);
+    const matchJsonRef = ref(storage, `Userdata/${user.uid}/match.json`);
+    const matchJsonURL = await getDownloadURL(matchJsonRef);
+    const response = await fetch(matchJsonURL);
+    let matchJson = await response.json();
 
-       // Upload the updated match.json file
-       const updatedMatchJsonRef = ref(storage, `Userdata/${user.uid}/match.json`);
-       const updatedMatchJsonBlob = new Blob([JSON.stringify(matchJson)], {
-         type: "application/json",
-       });
-       const uploadTask = uploadBytesResumable(updatedMatchJsonRef, updatedMatchJsonBlob);
-       uploadTask.on(
-         "state_changed",
-         (snapshot) => {
-           // Handle the upload progress here
-         },
-         (error) => {
-           // Handle unsuccessful uploads here
-         },
-         async () => {
-           // Handle successful uploads on complete
-           console.log("Deleted match and updated match.json successfully");
-           // Refresh the component to render the <Document>
-           fetchFile(file.name);
-         }
-       );
-     } else {
-       console.log("No match found for deletion.");
-     }
-   };
+    // Find the index of the matching object
+    const matchIndex = matchJson.findIndex((item) => item.pdf === file.name);
+    if (matchIndex !== -1) {
+      const match = matchJson[matchIndex];
+      // If a cover image is associated, delete it
+      if (match.cover !== "null") {
+        const imageRef = ref(storage, `PDFs/${user.uid}/covers/${match.cover}`);
+        await deleteObject(imageRef);
+      }
+
+      // Remove the matching object from the matchJson array
+      matchJson.splice(matchIndex, 1);
+
+      // Upload the updated match.json file
+      const updatedMatchJsonRef = ref(
+        storage,
+        `Userdata/${user.uid}/match.json`
+      );
+      const updatedMatchJsonBlob = new Blob([JSON.stringify(matchJson)], {
+        type: "application/json",
+      });
+      const uploadTask = uploadBytesResumable(
+        updatedMatchJsonRef,
+        updatedMatchJsonBlob
+      );
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          // Handle the upload progress here
+        },
+        (error) => {
+          // Handle unsuccessful uploads here
+        },
+        async () => {
+          // Handle successful uploads on complete
+          console.log(
+            "Deleted match, associated image (if present), and updated match.json successfully"
+          );
+          // Refresh the component to render the <Document>
+          fetchFile(file.name);
+        }
+      );
+    } else {
+      console.log("No match found for deletion.");
+    }
+    console.log("Deleted match and updated match.json successfully");
+    // Refresh the component to render the <Document>
+    fetchFile(file.name);
+  };
 
   const deleteCover = async () => {
     // Find the matching object in match.json
@@ -171,59 +189,67 @@ export default function Preview({
     }
   };
 
- const handleCoverUpload = async (event) => {
-   const image = event.target.files[0]; // The image file selected by the user
-   if (!image) return;
- 
-   // Download and parse match.json
-   const matchJsonRef = ref(storage, `Userdata/${user.uid}/match.json`);
-   const matchJsonURL = await getDownloadURL(matchJsonRef);
-   const response = await fetch(matchJsonURL);
-   let matchJson = await response.json();
- 
-   // Find the matching object and update the "cover" attribute
-   const match = matchJson.find(item => item.pdf === file.name); // Assuming 'file' is the PDF file being previewed
-   if (match) {
-     match.cover = image.name; // Update the cover attribute with the new image name
- 
-     // Upload the image
-     const imageRef = ref(storage, `PDFs/${user.uid}/covers/${image.name}`);
-     const uploadTask = uploadBytesResumable(imageRef, image);
-     uploadTask.on(
-       "state_changed",
-       (snapshot) => {
-         // Handle the upload progress here
-       },
-       (error) => {
-         // Handle unsuccessful uploads here
-       },
-       async () => {
-         // Handle successful uploads on complete
-         console.log("Image uploaded successfully");
- 
-         // Upload the updated match.json file
-         const updatedMatchJsonRef = ref(storage, `Userdata/${user.uid}/match.json`);
-         const updatedMatchJsonBlob = new Blob([JSON.stringify(matchJson)], { type: 'application/json' });
-         const uploadTask2 = uploadBytesResumable(updatedMatchJsonRef, updatedMatchJsonBlob);
-         uploadTask2.on(
-           "state_changed",
-           (snapshot) => {
-             // Handle the upload progress here
-           },
-           (error) => {
-             // Handle unsuccessful uploads here
-           },
-           async () => {
-             // Handle successful uploads on complete
-             console.log("match.json updated successfully");
-             // Call fetchFiles to refresh the list of files
-             fetchFile(file.name);
-           }
-         );
-       }
-     );
-   }
- };
+  const handleCoverUpload = async (event) => {
+    const image = event.target.files[0]; // The image file selected by the user
+    if (!image) return;
+
+    // Download and parse match.json
+    const matchJsonRef = ref(storage, `Userdata/${user.uid}/match.json`);
+    const matchJsonURL = await getDownloadURL(matchJsonRef);
+    const response = await fetch(matchJsonURL);
+    let matchJson = await response.json();
+
+    // Find the matching object and update the "cover" attribute
+    const match = matchJson.find((item) => item.pdf === file.name); // Assuming 'file' is the PDF file being previewed
+    if (match) {
+      match.cover = image.name; // Update the cover attribute with the new image name
+
+      // Upload the image
+      const imageRef = ref(storage, `PDFs/${user.uid}/covers/${image.name}`);
+      const uploadTask = uploadBytesResumable(imageRef, image);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          // Handle the upload progress here
+        },
+        (error) => {
+          // Handle unsuccessful uploads here
+        },
+        async () => {
+          // Handle successful uploads on complete
+          console.log("Image uploaded successfully");
+
+          // Upload the updated match.json file
+          const updatedMatchJsonRef = ref(
+            storage,
+            `Userdata/${user.uid}/match.json`
+          );
+          const updatedMatchJsonBlob = new Blob([JSON.stringify(matchJson)], {
+            type: "application/json",
+          });
+          const uploadTask2 = uploadBytesResumable(
+            updatedMatchJsonRef,
+            updatedMatchJsonBlob
+          );
+          uploadTask2.on(
+            "state_changed",
+            (snapshot) => {
+              // Handle the upload progress here
+            },
+            (error) => {
+              // Handle unsuccessful uploads here
+            },
+            async () => {
+              // Handle successful uploads on complete
+              console.log("match.json updated successfully");
+              // Call fetchFiles to refresh the list of files
+              fetchFile(file.name);
+            }
+          );
+        }
+      );
+    }
+  };
 
   const loading = () => {
     return (
@@ -245,21 +271,21 @@ export default function Preview({
       {showEditOverlay && (
         <div className={styles.overlay}>
           <div className={styles.buttonContainer}>
-          <button className={styles.button} onClick={triggerFileSelection}>
-            Upload Cover
-            <input
-              type="file"
-              ref={fileInputRef}
-              accept="image/jpeg, image/png, image/webp"
-              style={{ display: "none" }}
-              onChange={handleCoverUpload}
-            />
-          </button>
-          {coverImageURL && (
-            <button className={styles.button} onClick={deleteCover}>
-              Delete Cover
+            <button className={styles.button} onClick={triggerFileSelection}>
+              Upload Cover
+              <input
+                type="file"
+                ref={fileInputRef}
+                accept="image/jpeg, image/png, image/webp"
+                style={{ display: "none" }}
+                onChange={handleCoverUpload}
+              />
             </button>
-          )}
+            {coverImageURL && (
+              <button className={styles.button} onClick={deleteCover}>
+                Delete Cover
+              </button>
+            )}
           </div>
         </div>
       )}
