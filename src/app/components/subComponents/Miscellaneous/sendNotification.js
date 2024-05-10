d7fe815c91870be0ff92d7ee993fff4a532530b9
nothowstorygoes <esposito.pioalessandro@gmail.com>
2024-05-10 17:06:38 +0200

    Final Update

--- src/app/components/subComponents/Miscellaneous/sendNotification.js
+++ src/app/components/subComponents/Miscellaneous/sendNotification.js
@@ -1,15 +1,8 @@
 export default function sendNotification() {
     if(!("Notification" in window))
         alert("This browser doesn't support notifications :(");
-    else if (Notification.permission == "granted")
-        {
-            var notification = new Notification("Yay! Your slide are ready!",
-                {
-                    body: "Your upload was completed successfully.",
-                    vibrate: [200,100,200],
-                    image: "/notes-in-cloud/icons/logo/ico192.png"
-                }
-            )
+    else {
+
             Notification.requestPermission().then((result => {
                 if(result=="granted")
                     {
