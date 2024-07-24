187770395b809295ef7948b5c89734ed6588d5fc
nothowstorygoes <esposito.pioalessandro@gmail.com>
2024-07-24 17:37:57 +0200

    Update deploy.yml and firebase configuration with environment variables for Google API keys

--- src/app/firebase.tsx
+++ src/app/firebase.tsx
@@ -7,12 +7,12 @@ import { getStorage } from "firebase/storage";
 // Firebase configuration 
 
 const firebaseConfig = {
-  apiKey: process.env.GOOGLEAPIKEY,
-  authDomain: process.env.GOOGLEAUTHDOMAIN,
-  projectId: process.env.GOOGLEPROJECTID,
-  storageBucket: process.env.GOOGLESTORAGEBUCKET,
-  messagingSenderId: process.env.GOOGLEMESSAGINGSENDERID,
-  appId: process.env.GOOGLEAPPID
+  apiKey: process.env.NEXT_PUBLIC_GOOGLEAPIKEY,
+  authDomain: process.env.NEXT_PUBLIC_GOOGLEAUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_GOOGLEPROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_GOOGLESTORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_GOOGLEMESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_GOOGLEAPPID
 };
 
 
