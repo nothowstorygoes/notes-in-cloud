6809fa32188d66edd6c0537b2e6a4021f52af8ef
nothowstorygoes <esposito.pioalessandro@gmail.com>
2024-07-24 16:17:48 +0200

    chore: Update firebase configuration with environment variables

--- src/app/firebase.tsx
+++ src/app/firebase.tsx
@@ -7,12 +7,12 @@ import { getStorage } from "firebase/storage";
 // Firebase configuration 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA_KUvTgSF421b9qljbujDg4bwJeTmPzME",
-  authDomain: "my-notes-cloud.firebaseapp.com",
-  projectId: "my-notes-cloud",
-  storageBucket: "my-notes-cloud.appspot.com",
-  messagingSenderId: "456386487677",
-  appId: "1:456386487677:web:dd1cfaa9d73a10f1e2d814"
+  apiKey: process.env.GOOGLEAPIKEY,
+  authDomain: process.env.GOOGLEAUTHDOMAIN,
+  projectId: process.env.GOOGLEPROJECTID,
+  storageBucket: process.env.GOOGLESTORAGEBUCKET,
+  messagingSenderId: process.env.GOOGLEMESSAGINGSENDERID,
+  appId: process.env.GOOGLEAPPID
 };
 
 
